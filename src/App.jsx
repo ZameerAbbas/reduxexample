deeaaa1f63f77c1d3598aa878696deedb61465a7
ZameerAbbas <zameerabbas3435@gmail.com>
2023-11-14 14:21:58 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='bg-emerald-600 p-0 m-0'>
+   <h1 className='text-5xl'>
+    vite app for redux
+   </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
