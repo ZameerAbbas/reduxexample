a140f6764f271f7d558b2945aac9aa5df1fd82bb
ZameerAbbas <zameerabbas3435@gmail.com>
2023-11-15 16:08:32 +0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
+import Task from "./components/Task";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
-    <>
-      <div className="bg-emerald-600 p-0 m-0 flex">
-        <h1 className="text-5xl">vite app for redux</h1>
-      </div>
-    </>
+    <div className="App">
+      <Task />
+      <TaskList />
+    </div>
   );
 }
 
