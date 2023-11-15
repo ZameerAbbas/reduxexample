47cc47f2971f1e01aaf99de8d0a2e5b8a7e20390
ZameerAbbas <zameerabbas3435@gmail.com>
2023-11-15 15:19:13 +0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  
   return (
     <>
-      <div className='bg-emerald-600 p-0 m-0'>
-   <h1 className='text-5xl'>
-    vite app for redux
-   </h1>
+      <div className="bg-emerald-600 p-0 m-0 flex">
+        <h1 className="text-5xl">vite app for redux</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
